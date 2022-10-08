8343b37e8f3ffc18495ba5705d11a64a06c7bd7e
Sebastian Schindler <sebastian@schindlertai.de>
2022-10-09 00:55:48 +0200

    AP-5-HTTPServer
    add: HTTP Server
    add: index.html für leeren HTTP Call
    add: IOException zur MainMethod hinzugefügt
    add: HTTP Checks zur .gitignore hinzugefügt
    change: readme.md verschönert

--- src/main/java/org/devcloud/ap/Azubiprojekt.java
+++ src/main/java/org/devcloud/ap/Azubiprojekt.java
@@ -1,13 +1,11 @@
 package org.devcloud.ap;
 
-import lombok.Getter;
-import org.devcloud.ap.database.SQLPostgres;
+import org.devcloud.ap.utils.HTTPServer;
+
+import java.io.IOException;
 
 public class Azubiprojekt {
-
-    @Getter static SQLPostgres sqlPostgres;
-
-    public static void main(String[] args) {
-        sqlPostgres = new SQLPostgres();
+    public static void main(String[] args) throws IOException {
+        HTTPServer.startServer();
     }
 }
