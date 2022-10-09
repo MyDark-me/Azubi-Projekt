f2ed39b54d641393e5faa6b3f7c241e084c6e30b
Keksgauner <paul@rakutt.eu>
2022-10-09 19:53:23 +0200

    AP-16-ROLE-API
    ### Updated
    Role die write Info wurde zusammen in eine Methode gepackt

--- src/main/java/org/devcloud/ap/apicalls/Role.java
+++ src/main/java/org/devcloud/ap/apicalls/Role.java
@@ -9,28 +9,52 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 
 public class Role {
-    private static final Logger logger = LoggerFactory.getLogger(Base.class);
+    private static final Logger logger = LoggerFactory.getLogger(Role.class);
     private static final JSONCreator jsonCreator = new JSONCreator().addKeys("statuscode");
 
     public static void register(HttpServer httpServer) {
+        httpServer.createContext("/api/role/create", new Create());
         httpServer.createContext("/api/role/delete", new Delete());
         httpServer.createContext("/api/role/users", new Users());
-        httpServer.createContext("/api/role/login", new Login());
     }
 
     private Role() {}
 
     private static void addResponseHeaders(HttpExchange httpExchange) {
-        String request = httpExchange.getRequestURI().toString();
         httpExchange.getResponseHeaders().add("Content-Type", "application/json");
-
-        debugRequest(logger, request);
     }
 
-    private static void debugRequest(Logger logger, String request) {
-        logger.debug("{} - was requested", request);
+    private static void writeResponse(HttpExchange httpExchange, String response) throws IOException {
+        httpExchange.sendResponseHeaders(200, response.length());
+
+        OutputStream outputStream = httpExchange.getResponseBody();
+        for(char write : response.toCharArray())
+            outputStream.write(write);
+        outputStream.close();
+    }
+
+    private static void debugRequest(URI requestURI) {
+        logger.debug("{} - was requested", requestURI);
+    }
+
+    private static class Create implements HttpHandler {
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            addResponseHeaders(httpExchange);
+
+            // name / color / token
+            URI requestURI = httpExchange.getRequestURI();
+            debugRequest(requestURI);
+
+            String response = jsonCreator
+                    .addKeys("response")
+                    .addValue(201, "User API is not implemented yet!").toString();
+
+            writeResponse(httpExchange, response);
+        }
     }
 
     private static class Delete implements HttpHandler {
@@ -38,15 +62,15 @@ public class Role {
         public void handle(HttpExchange httpExchange) throws IOException {
             addResponseHeaders(httpExchange);
 
-            String response = new JSONCreator()
-                    .addKeys("statuscode", "response")
+            // name / token
+            URI requestURI = httpExchange.getRequestURI();
+            debugRequest(requestURI);
+
+            String response = jsonCreator
+                    .addKeys("response")
                     .addValue(201, "User API is not implemented yet!").toString();
 
-            httpExchange.sendResponseHeaders(200, response.length());
-
-            OutputStream outputStream = httpExchange.getResponseBody();
-            outputStream.write(response.getBytes());
-            outputStream.close();
+            writeResponse(httpExchange, response);
         }
     }
 
@@ -55,32 +79,15 @@ public class Role {
         public void handle(HttpExchange httpExchange) throws IOException {
             addResponseHeaders(httpExchange);
 
-            String response = new JSONCreator()
-                    .addKeys("statuscode", "response")
+            // name / token
+            URI requestURI = httpExchange.getRequestURI();
+            debugRequest(requestURI);
+
+            String response = jsonCreator
+                    .addKeys("response")
                     .addValue(201, "User API is not implemented yet!").toString();
 
-            httpExchange.sendResponseHeaders(200, response.length());
-
-            OutputStream outputStream = httpExchange.getResponseBody();
-            outputStream.write(response.getBytes());
-            outputStream.close();
-        }
-    }
-
-    private static class Login implements HttpHandler {
-        @Override
-        public void handle(HttpExchange httpExchange) throws IOException {
-            addResponseHeaders(httpExchange);
-
-            String response = new JSONCreator()
-                    .addKeys("statuscode", "response")
-                    .addValue(201, "User API is not implemented yet!").toString();
-
-            httpExchange.sendResponseHeaders(200, response.length());
-
-            OutputStream outputStream = httpExchange.getResponseBody();
-            outputStream.write(response.getBytes());
-            outputStream.close();
+            writeResponse(httpExchange, response);
         }
     }
 }
