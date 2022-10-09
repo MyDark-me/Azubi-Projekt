6b0128ad6a9147da79e91b76ee3b74c2de79b368
Sebastian Schindler <87519233+DarkModz-Official@users.noreply.github.com>
2022-10-09 16:33:38 +0200

    AP-21-Beautify-index-File (#5)
    * AP-20-Codesmell-entfernung
    
    doppelter import von IOException
    index.html
    log4j.properties / console sauber halten
    logger start server info.
    variable name reqest zu request geändert

--- src/main/java/org/devcloud/ap/Azubiprojekt.java
+++ src/main/java/org/devcloud/ap/Azubiprojekt.java
@@ -24,5 +24,6 @@ public class Azubiprojekt {
             exception.printStackTrace();
         }
         sqlPostgres = new SQLPostgres("localhost:5432", "postgres", "password", "azubiprojekt");
+
     }
 }
